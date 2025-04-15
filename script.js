862fa1da2f66c090edcfe3017fe5cf5ac4c67b2a
ashish <ashishmasih92g@gmail.com>
2025-04-16 00:21:38 +0530

    assignment

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+
+let line = document.querySelector("#line")
+
+let angel=0
+
+ setInterval(()=>{
+angel+=2
+    line.style.transform = `rotate(${angel}deg)`
+},20)
